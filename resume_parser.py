d15411c81f539faf467b286a45bb50a29f344c68
gowthampreetham2020-collab <gowthampreetham2020@gmail.com>
2026-03-22 20:17:57 +0530

    Update resume_parser.py

--- resume_parser.py
+++ resume_parser.py
@@ -1,16 +1,10 @@
 import pdfplumber
 
 def parse_resume(file):
-
     text = ""
-
     with pdfplumber.open(file) as pdf:
-
         for page in pdf.pages:
-
             page_text = page.extract_text()
-
             if page_text:
                 text += page_text
-
     return text
